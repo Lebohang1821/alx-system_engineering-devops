1f64d29fcc4bc9434030963169f8ad631a38ad6b
Lebohang1821 <millboylebohang@gmail.com>
2023-12-23 04:16:27 -0800

    task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # It installs flask using pip3
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
