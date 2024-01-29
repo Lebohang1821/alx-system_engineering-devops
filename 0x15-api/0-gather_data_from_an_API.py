d2be6bce06ac8c7707a6e10777606f4708aa0297
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 04:26:02 -0800

    task-0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,28 +3,16 @@
 import requests
 from sys import argv
 
+
 if __name__ == '__main__':
-    # Fetch TODO list data from the API
-    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todo_data = todo_data.json()
-
-    # Fetch user data from the API
-    users_data = requests.get('https://jsonplaceholder.typicode.com/users')
-    users_data = users_data.json()
-
-    # Extract the user information based on the provided employee ID
-    user = [i for i in users_data if i.get('id') == int(argv[1])][0]
-
-    # Filter tasks based on the provided employee ID
-    tasks = [i for i in todo_data if i.get('userId') == int(argv[1])]
-
-    # Filter completed tasks
-    completed_tasks = [i for i in tasks if i.get('completed') is True]
-
-    # Display the TODO list progress information
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    user = [i for i in users if i.get('id') == int(argv[1])][0]
+    tasks = [i for i in data if i.get('userId') == int(argv[1])]
+    completed = [i for i in tasks if i.get('completed') is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get('name'), len(completed_tasks), len(tasks)))
-
-    # Display the titles of completed tasks
-    for task in completed_tasks:
+        user.get('name'), len(completed), len(tasks)))
+    for task in completed:
         print("\t {}".format(task.get('title')))
