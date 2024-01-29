a302e6d0ca1e5c98a854561ad33d1acb09f1c831
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 05:01:48 -0800

    API-01

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,31 +4,17 @@ import csv
 import requests
 from sys import argv
 
-# Fetch todo data from the JSONPlaceholder API
-data = requests.get('https://jsonplaceholder.typicode.com/todos')
-data = data.json()
 
-# Fetch user data from the JSONPlaceholder API
-users = requests.get('https://jsonplaceholder.typicode.com/users')
-users = users.json()
-
-# Find the user based on the provided user ID
-user = [i for i in users if i.get('id') == int(argv[1])][0]
-
-# Filter tasks based on the provided user ID
-tasks = [i for i in data if i.get('userId') == int(argv[1])]
-
-# Create a CSV file with the provided user ID as the filename
-with open(argv[1] + '.csv', 'w', newline='') as f:
-    # Create a CSV writer with quoting set to QUOTE_NONNUMERIC
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-
-    # Write headers to the CSV file
-    writer.writerow(['UserID', 'Username', 'Completed', 'Title'])
-
-    # Write each task's information to the CSV file
-    for task in tasks:
-        writer.writerow([str(argv[1]), user.get('username'),
-                         str(task.get('completed')), task.get('title')])
-    # Close the file
-    f.close()
+if __name__ == '__main__':
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    user = [i for i in users if i.get('id') == int(argv[1])][0]
+    tasks = [i for i in data if i.get('userId') == int(argv[1])]
+    with open(argv[1] + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        for task in tasks:
+            writer.writerow([str(argv[1]), user.get('username'),
+                             str(task.get('completed')), task.get('title')])
+        f.close()
