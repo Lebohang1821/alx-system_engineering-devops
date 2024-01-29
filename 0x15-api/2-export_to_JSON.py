ae0c416abb27417c2b68e1764d1d230d0b54fbc6
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 05:04:42 -0800

    API-02

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-'''extend your Python script to export data in the JSON format.'''
+'''It extends my Python script to export data in the JSON format.'''
 import json
 import requests
 from sys import argv
-
 
 if __name__ == '__main__':
     data = requests.get('https://jsonplaceholder.typicode.com/todos')
@@ -12,11 +11,10 @@ if __name__ == '__main__':
     users = users.json()
     user = [i for i in users if i.get('id') == int(argv[1])][0]
     tasks = [i for i in data if i.get('userId') == int(argv[1])]
+
     with open(argv[1] + '.json', 'w', newline='') as f:
         expo = {argv[1]: []}
-        l = expo.get(argv[1])
+        task_list = expo.get(argv[1])
         for task in tasks:
-            l.append({"task": task.get('title'), "completed": task.
-                      get('completed'), "username": user.get('username')})
+            task_list.append({"task": task.get('title'), "completed": task.get('completed'), "username": user.get('username')})
         f.write(json.dumps(expo))
-        f.close()
