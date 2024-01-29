1a95eabe1c84e734e17a475dd0b1e5039c331ff6
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 05:00:41 -0800

    API-01

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,32 +4,31 @@ import csv
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    # Fetching todo data from JSONPlaceholder API
-    data = requests.get('https://jsonplaceholder.typicode.com/todos')
-    data = data.json()
+# Fetch todo data from the JSONPlaceholder API
+data = requests.get('https://jsonplaceholder.typicode.com/todos')
+data = data.json()
 
-    # Fetching user data from JSONPlaceholder API
-    users = requests.get('https://jsonplaceholder.typicode.com/users')
-    users = users.json()
+# Fetch user data from the JSONPlaceholder API
+users = requests.get('https://jsonplaceholder.typicode.com/users')
+users = users.json()
 
-    # Extracting user information based on the provided user ID from command line argument
-    user = [i for i in users if i.get('id') == int(argv[1])][0]
+# Find the user based on the provided user ID
+user = [i for i in users if i.get('id') == int(argv[1])][0]
 
-    # Filtering tasks based on the provided user ID
-    tasks = [i for i in data if i.get('userId') == int(argv[1])]
+# Filter tasks based on the provided user ID
+tasks = [i for i in data if i.get('userId') == int(argv[1])]
 
-    # Writing data to a CSV file
-    with open(argv[1] + '.csv', 'w', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+# Create a CSV file with the provided user ID as the filename
+with open(argv[1] + '.csv', 'w', newline='') as f:
+    # Create a CSV writer with quoting set to QUOTE_NONNUMERIC
+    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
 
-        # Writing header row
-        writer.writerow(['User ID', 'Username', 'Completed', 'Title'])
+    # Write headers to the CSV file
+    writer.writerow(['UserID', 'Username', 'Completed', 'Title'])
 
-        # Writing data for each task
-        for task in tasks:
-            writer.writerow([str(argv[1]), user.get('username'),
-                             str(task.get('completed')), task.get('title')])
-
-        # Closing the file
-        f.close()
+    # Write each task's information to the CSV file
+    for task in tasks:
+        writer.writerow([str(argv[1]), user.get('username'),
+                         str(task.get('completed')), task.get('title')])
+    # Close the file
+    f.close()
