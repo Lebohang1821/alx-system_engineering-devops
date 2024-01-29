f87b8f5ed25589f87dffe493b9764775acee8df2
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 04:29:37 -0800

    task-1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''returns information about TODO list progress by employee id.'''
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    user = [i for i in users if i.get('id') == int(argv[1])][0]
+    tasks = [i for i in data if i.get('userId') == int(argv[1])]
+    with open(argv[1] + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        for task in tasks:
+            writer.writerow([str(argv[1]), user.get('username'),
+                             str(task.get('completed')), task.get('title')])
+        f.close()
