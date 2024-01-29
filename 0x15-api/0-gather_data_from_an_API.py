6faa9450fcf2cb762ca8db6cb5aaea23f5eb7e7b
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 04:51:49 -0800

    API-01

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
 '''It returns information about his/her TODO list progress'''
 import requests
-import sys
+from sys import argv
 
-def get_employee_todo_progress(employee_id):
-    # API endpoint for fetching employee information
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+if __name__ == '__main__':
+    # Fetching todo data from JSONPlaceholder API
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
 
-    # Fetch employee information
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        user_data = user_response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching employee information: {e}")
-        sys.exit(1)
+    # Fetching user data from JSONPlaceholder API
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
 
-    # Fetch TODO list for the employee
-    try:
-        todos_response = requests.get(todos_url)
-        todos_response.raise_for_status()
-        todos_data = todos_response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching TODO list: {e}")
-        sys.exit(1)
+    # Extracting user information based on the provided user ID from command line argument
+    user = [i for i in users if i.get('id') == int(argv[1])][0]
 
-    # Calculate TODO list progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+    # Filtering tasks based on the provided user ID
+    tasks = [i for i in data if i.get('userId') == int(argv[1])]
 
-    # Display progress information
-    print(f"Employee {user_data['name']} is done with tasks({completed_tasks}/{total_tasks}):")
-    for todo in todos_data:
-        if todo['completed']:
-            print(f"\t{todo['title']}")
+    # Filtering completed tasks
+    completed = [i for i in tasks if i.get('completed') is True]
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
-        sys.exit(1)
+    # Printing a summary of completed tasks for the specified user
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completed), len(tasks)))
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    # Printing details of each completed task
+    for task in completed:
+        print("\t {}".format(task.get('title')))
