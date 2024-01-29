849d7566dff5fe690d7cb5118ffdc08192a0fb8c
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 04:55:33 -0800

    API-01

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-'''It export data in the CSV format.'''
+'''It exports data in the CSV format.'''
 import csv
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
+    # Fetching todo data from JSONPlaceholder API
     data = requests.get('https://jsonplaceholder.typicode.com/todos')
     data = data.json()
+
+    # Fetching user data from JSONPlaceholder API
     users = requests.get('https://jsonplaceholder.typicode.com/users')
     users = users.json()
+
     user = [i for i in users if i.get('id') == int(argv[1])][0]
+
+    # Filtering tasks based on the provided user ID
     tasks = [i for i in data if i.get('userId') == int(argv[1])]
+
+    # Writing data to a CSV file
     with open(argv[1] + '.csv', 'w', newline='') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+
+        # Writing header row
+        writer.writerow(['User ID', 'Username', 'Completed', 'Title'])
+
+        # Writing data for each task
         for task in tasks:
             writer.writerow([str(argv[1]), user.get('username'),
                              str(task.get('completed')), task.get('title')])
+
+        # Closing the file
         f.close()
