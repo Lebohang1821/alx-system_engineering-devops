79e07fa035af0a7b3db18f46df8bcc23dff3ff6b
Lebohang1821 <millboylebohang@gmail.com>
2024-02-14 19:17:43 -0800

    debur

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A_ Puppet code to fix the Apache 500 Internal Server Error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
