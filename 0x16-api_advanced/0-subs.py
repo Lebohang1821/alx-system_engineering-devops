948ee06504f59dcd3d9e11d4838f725cc0f9fe95
Lebohang1821 <millboylebohang@gmail.com>
2024-02-16 21:37:04 -0800

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
-
+from json.decoder import JSONDecodeError
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    '''
+        It returns number of subscribers for given subreddit
+    '''
+    user_agent = {'User-Agent': 'Lizzie'}
+    try:
+        response = requests.get(
+            f'https://www.reddit.com/r/{subreddit}/about.json',
+            headers=user_agent
+        )
+        response.raise_for_status()  # Raise an exception for bad status codes
+
+        # Check if response contains valid JSON data
+        try:
+            data = response.json()
+        except JSONDecodeError:
+            print("Subreddit not found or private.")
+            return 0
+
+        # Check if the response contains valid data structure
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            print("Invalid response format.")
+            return 0
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return 0  # Return 0 if there's an issue with the request
