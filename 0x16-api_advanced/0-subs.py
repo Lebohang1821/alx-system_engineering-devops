1552296f5a7fc480887eae35fdeba45c1eb87b39
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-02-17 07:15:10 +0200

    Updat

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,14 @@
 #!/usr/bin/python3
-'''
-    It contains function number_of_subscribers
-'''
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''
-        It returns number of subscribers for given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    try:
-        url = requests.get(
-            'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-            headers=user
-        )
-        url.raise_for_status()  # Raise an exception for bad status codes
-        data = url.json()
-        return data['data']['subscribers']
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return 0  # Return 0 if there's an issue with the request
-    except KeyError:
-        print("Subreddit not found or private.")
-        return 0  # Return 0 for non-existing or private subreddit
-
-
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python script_name.py subreddit_name")
-    else:
-        subscribers = number_of_subscribers(argv[1])
-        print("Number of subscribers:", subscribers)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    with requests.session() as client:
+        info = client.get(url, headers=headers, allow_redirects=False).json()
+        try:
+            return info.get('data', {}).get('subscribers', 0)
+        except Exception:
+            return 0
