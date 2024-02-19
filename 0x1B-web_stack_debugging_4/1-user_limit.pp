4e4a77bca02999eeddc27e0cca5c5483be934800
Lebohang1821 <millboylebohang@gmail.com>
2024-02-19 08:29:15 -0800

    task-1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# It enable user to login and open files without error
+
+# Increase hard file limit for Holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# It increase soft file limit
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
